8426dfcb4e74b09cf17b74593a14ad31ee10c0ff
Frank Tran <ftransd@gmail.com>
2017-09-27 17:33:15 -0700

    Change syntax to support es6 module support

--- neumatico.js
+++ neumatico.js
@@ -1,3 +1,5 @@
-setInterval(function() {
+export default function(){
+  setInterval(function() {
     document.documentElement.style.cssText = "height:100%;width:100%;overflow:auto;transform: rotate("+Date.now()%360+"deg);";
-},10);
+  },10);
+}
